3db17fc4eb1abe6de818ad56e5ea18090b80f854
kcL1211 <kcL1211>
2016-10-11 19:50:39 -0700

    starting new Gui

---
+++ src/com/company/SwingGui.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by Casey on 10/11/16.
+ */
+public class SwingGui implements Gui {
+    void instructions();
+
+    void visuals();
+
+    int getInput(int color);
+
+    void showBoard(Board b);
+
+
+}
